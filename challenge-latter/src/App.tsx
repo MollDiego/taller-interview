8b5124977adc04b1ab27928d912689f5ab75e297
Diego Moll <dmoll217@gmail.com>
2026-06-10 19:23:33 -0300

    add some frontend fix

--- challenge-latter/src/App.tsx
+++ challenge-latter/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(null);
   const [description, setDescription] = useState("");
   const [showData, setShowData] = useState(false);
 
@@ -12,24 +12,31 @@ function App() {
   } 
 
   return (
-    <>
-    <form onSubmit={handleSubmit}>
-      <input
-        type='number'
-        onChange={(e) => setAmount(Number(e?.target?.value))}
-        value={amount}/>
-      <input
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}/>
-      <button type='submit'>Submit</button>
-    </form>
-    {showData ? 
-    <div>
-      <div>{amount}</div>
-      <div>{description}</div>
+    <div className='app-container'>
+      <form onSubmit={handleSubmit} className='form-container'>
+        <input
+          className='form-input'
+          id='amount'
+          name='amount'
+          type='number'
+          onChange={(e) => setAmount(Number(e?.target?.value))}
+          value={amount}/>
+        <input
+          className='form-input'
+          id='description'
+          name='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}/>
+        <button type='submit'>Submit</button>
+      </form>
+      {showData ? 
+      <div>
+        <h2>transaction details</h2>
+        <div>Amount: {amount}</div>
+        <div>Description: {description}</div>
+      </div>
+      : ''}
     </div>
-    : ''}
-    </>
   )
 }
 
