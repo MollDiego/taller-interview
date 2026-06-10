71b4d9d60518da692d6b033908f2ab811ffa77ef
Diego Moll <dmoll217@gmail.com>
2026-06-10 19:48:57 -0300

    fix post service and add get service

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,40 @@
-const express = require('express')
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let transaction = [];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Taller challenge - Backend!')
 })
 
 app.post('/transaction', (req, res) => {
   const { amount, description } = req.body;
-  console.log(req.body)
+  const newTransaction = {
+    id: uuidv4(),
+    amount,
+    description
+  }
     
     if (!amount || !description) {
         return res.status(400).json({ error: 'amount and description are required' });
     }
 
-    // Process the data (e.g., save to a database)
+    transaction.push(newTransaction);
     res.status(201).json({
-        message: 'User created successfully',
-        data: { amount, description }
+        message: 'Transaction created successfully',
+        data: newTransaction
     });
 });
 
+app.get('/transaction', (req, res) => {
+    res.json(transaction);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Transaction app listening on port ${port}`)
 })
